2fc20d85f61f4a6174193282a03a157057346285
Valtteri Virtanen <valtteri.virtane@gmail.com>
2018-12-22 21:16:20 +0200

    huge commit to break it all. Got rid of templates and added some jwt stuff and gorilla/mux

--- main.go
+++ main.go
@@ -4,26 +4,18 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"os"
-	"path"
 	"time"
 
 	"golang.org/x/crypto/bcrypt"
 
 	_ "github.com/lib/pq"
 
-	//"github.com/gorilla/mux"
 	"github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/mux"
 )
-
-type Page struct {
-	Title            string
-	Heading1         string
-	NotAuthenticated bool
-}
 
 type User struct {
 	ID       int    `json:"id"`
@@ -47,17 +39,13 @@ type CustomClaims struct {
 	jwt.StandardClaims
 }
 
-const secret = "tätä.ei_kukaan|tosta,v44nvoiarvata?EIHÄN!?"
+const secret = "tÃ¤tÃ¤.ei_kukaan|tosta,v44nvoiarvata?EIHÃ„N!?"
 
-var tpl *template.Template
-var signuptpl *template.Template
 var db *sql.DB
 
 func init() {
-	tpl = template.Must(template.ParseFiles("templates/tpl.gohtml"))
-	signuptpl = template.Must(template.ParseFiles("templates/signup.gohtml"))
 	err := error(nil)
-	db, err = sql.Open("postgres", "postgres://**********")
+	db, err = sql.Open("postgres", os.Getenv("DATABASE_URL"))
 
 	if err != nil {
 		panic(err)
@@ -66,7 +54,7 @@ func init() {
 	if err = db.Ping(); err != nil {
 		panic(err)
 	}
-	fmt.Println("You connected to your database.")
+	fmt.Println("Connection to database established.")
 
 }
 
@@ -76,77 +64,37 @@ func main() {
 	if port == "" {
 		port = "8000"
 	}
-	//rtr := mux.NewRouter()
+	rtr := mux.NewRouter()
 
-	http.HandleFunc("/", index)
-	http.HandleFunc("/login", loginHandler)
-	http.HandleFunc("/loggedIn", loggedIn)
-	http.HandleFunc("/signUp", signUpHandler)
-	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css/"))))
+	rtr.HandleFunc("/api/get-token", getToken).Methods("POST")
 
-	http.HandleFunc("/api/users", usersHandler)
-	http.HandleFunc(`/api/users/1`, singleUserHandler)
-	/*
-		err := http.ListenAndServe(":"+port, nil)
-		if err != nil {
-			log.Fatal("ListenAndServe: ", err)
-		}
-	*/
+	rtr.HandleFunc("/api/users", getUsers).Methods("GET")
+	rtr.HandleFunc("/api/users", createUser).Methods("POST")
+	rtr.HandleFunc("/api/users/{id:[0-9]+}", getUser).Methods("GET")
+	rtr.HandleFunc("/api/users/{id:[0-9]+}", updateUser).Methods("PUT")
+	rtr.HandleFunc("/api/users/{id:[0-9]+}", deleteUser).Methods("DELETE")
 
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, rtr))
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		home := Page{
-			Title:            "My Login Page",
-			Heading1:         "Project login-page",
-			NotAuthenticated: true,
-		}
-
-		err := tpl.ExecuteTemplate(w, "tpl.gohtml", home)
-
-		checkIfError(err)
-	} else {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	}
-}
-
-func loggedIn(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		page := Page{
-			Title:    "Logged In",
-			Heading1: "Logged In Successfully",
-		}
-
-		err := tpl.ExecuteTemplate(w, "tpl.gohtml", page)
-
-		checkIfError(err)
-	} else {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	}
-}
-
-func loginHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
+func getToken(w http.ResponseWriter, r *http.Request) {
 
 		name := r.FormValue("name")
 		passwrd := r.FormValue("password")
-		redirectTo := "/"
 
 		if name != "" && passwrd != "" {
 
-			err := validateCredentials(name, passwrd)
-			if err == true {
-				//createToken(name)
-				redirectTo = "/loggedIn"
+			valid := validateCredentials(name, passwrd)
+			if valid == true {
+				token := createToken(name)
+				w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+				json.NewEncoder(w).Encode(token)
+			} else {
+				http.Error(w, "Invalid credentials", http.StatusForbidden)
 			}
 		}
 
-		http.Redirect(w, r, redirectTo, 302)
-	} else {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	}
 }
 
 func createToken(username string) *JWTToken {
@@ -162,68 +110,75 @@ func createToken(username string) *JWTToken {
 	return &JWTToken
 }
 
-func singleUserHandler(w http.ResponseWriter, r *http.Request) {
+func getUser(w http.ResponseWriter, r *http.Request) {
+	user := User{}
+	params := mux.Vars(r)
+
+	id := params["id"]
+
+	if len(id) == 0 {
+		http.Error(w, "User not found", http.StatusNotFound)
+	}
+	if err := db.QueryRow("SELECT id, email, username FROM users WHERE id=$1", id).Scan(&user.ID, &user.Email, &user.Username); err != nil {
+		log.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	json.NewEncoder(w).Encode(user)
+}
+
+func updateUser(w http.ResponseWriter, r *http.Request) {
 	user := User{}
 
-	switch r.Method {
-	case "PUT":
-		id := path.Base(r.URL.Path)
+	params := mux.Vars(r)
+	id := params["id"]
 
-		err := db.QueryRow("SELECT id, email, username FROM users WHERE id=$1", id).Scan(&user.ID, &user.Email, &user.Username)
+	err := db.QueryRow("SELECT id, email, username FROM users WHERE id=$1", id).Scan(&user.ID, &user.Email, &user.Username)
 
-		v := r.FormValue("username")
-		if len(v) > 0 {
-			user.Username = v
-		}
-
-		v = r.FormValue("email")
-		if len(v) > 0 {
-			user.Email = v
-		}
-
-		if err != nil {
-			log.Println(err)
-		} else {
-			_, err = db.Exec("UPDATE users SET email=$1, username=$2 WHERE id=$3", user.Email, user.Username, user.ID)
-		}
-
-		if err != nil {
-			log.Println(err)
-		}
-		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-
-		json.NewEncoder(w).Encode(user)
-	case "GET":
-		params := r.URL.Query()
-		id := params.Get("id")
-		//id := path.Base(r.URL.Path)
-		fmt.Println(params)
-		fmt.Println(r.URL.Query())
-		fmt.Println(r.URL.Path)
-		if len(id) == 0 {
-			http.Error(w, "User not found", http.StatusNotFound)
-		}
-		if err := db.QueryRow("SELECT id, email, username FROM users WHERE id=$1", id).Scan(&user.ID, &user.Email, &user.Username); err != nil {
-			log.Println(err)
-		}
-
-		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-
-		json.NewEncoder(w).Encode(user)
-	case "DELETE":
-		id := path.Base(r.URL.Path)
-
-		err := db.QueryRow("DELETE FROM users WHERE id=$1 RETURNING id,username,email", id).Scan(&user.ID, &user.Email, &user.Username)
-
-		if err != nil {
-			log.Println(err)
-		}
-		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-
-		json.NewEncoder(w).Encode(user)
-	default:
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	v := params["username"]
+	if len(v) > 0 {
+		user.Username = v
 	}
+
+	v = params["email"]
+	if len(v) > 0 {
+		user.Email = v
+	}
+
+	v = params["password"]
+	if len(v) > 0 {
+		user.Password = v
+	}
+
+	if err != nil {
+		log.Println(err)
+	} else {
+		_, err = db.Exec("UPDATE users SET email=$1, username=$2 WHERE id=$3", user.Email, user.Username, user.ID)
+	}
+
+	if err != nil {
+		log.Println(err)
+	}
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	json.NewEncoder(w).Encode(user)
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	user := User{}
+
+	params := mux.Vars(r)
+	id := params["id"]
+
+	err := db.QueryRow("DELETE FROM users WHERE id=$1 RETURNING id,username,email", id).Scan(&user.ID, &user.Email, &user.Username)
+
+	if err != nil {
+		log.Println(err)
+	}
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	json.NewEncoder(w).Encode(user)
 }
 
 func validateCredentials(name string, passwrd string) bool {
@@ -251,61 +206,50 @@ func validPassword(pwd string, hashPwd string) error {
 	return err
 }
 
-func signUpHandler(w http.ResponseWriter, r *http.Request) {
-
-	page := Page{
-		Title:    "Please Sign Up",
-		Heading1: "Sign Up Please",
-	}
-
-	err := signuptpl.ExecuteTemplate(w, "signup.gohtml", page)
-
-	checkIfError(err)
-}
-
 func checkIfError(e error) {
 	if e != nil {
 		log.Fatalln(e)
 	}
 }
 
-func usersHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		users, err := getUsers()
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	users, err := getUsersData()
 
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-
-		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-
-		json.NewEncoder(w).Encode(users)
-
-	case "POST":
-
-		name := r.FormValue("name")
-		email := r.FormValue("email")
-		passwrd := hashPwd([]byte(r.FormValue("password")))
-
-		userToBeCreated := User{-1, email, name, passwrd}
-
-		user, err := createUser(&userToBeCreated)
-
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-
-		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-
-		json.NewEncoder(w).Encode(user)
-
-	default:
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	json.NewEncoder(w).Encode(users)
 }
 
-func createUser(u *User) (*User, error) {
+func createUser(w http.ResponseWriter, r *http.Request) {
+	var u User
+	if r.Body == nil {
+		http.Error(w, "Please send a request body", 400)
+		return
+	}
+	err := json.NewDecoder(r.Body).Decode(&u)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	u.Password = hashPwd([]byte(u.Password))
+
+	user, err := createUserData(&u)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	json.NewEncoder(w).Encode(user)
+}
+
+func createUserData(u *User) (*User, error) {
 	_, err := db.Query("INSERT INTO users(username, email, password) VALUES ($1,$2,$3)", u.Username, u.Email, u.Password)
 
 	if err != nil {
@@ -321,7 +265,7 @@ func createUser(u *User) (*User, error) {
 	return &user, nil
 }
 
-func getUsers() ([]*User, error) {
+func getUsersData() ([]*User, error) {
 	rows, err := db.Query("SELECT id, email, username FROM users")
 
 	if err != nil {
